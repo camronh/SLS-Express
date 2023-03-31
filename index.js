2471965efb8a398bcbdc9e4dbf7a8f3137f20336
camronh <camronhaider@gmail.com>
2023-03-31 13:39:02 -0400

    write up

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const serverless = require("serverless-http");
 const express = require("express");
 const app = express();
 
+try {
+  require("dotenv").config();
+} catch (e) {
+  console.log("No .env file found");
+}
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
 app.get("/path/:pathParam", function (req, res) {
-  res.send("Hello World! We on V2!\n\n" + req.params.pathParam);
+  res.send(
+    "Hello World! We on V2!\n\n" +
+      req.params.pathParam +
+      "\n\n" +
+      process.env.API_KEY
+  );
 });
 
 module.exports.handler = serverless(app);
